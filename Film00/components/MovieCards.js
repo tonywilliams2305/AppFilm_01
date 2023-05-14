ee1290c64cfd01987ca138a156af355969cf012f
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-14 22:24:20 +0700

    Update MovieCards.js

--- Film00/components/MovieCards.js
+++ Film00/components/MovieCards.js
@@ -1,9 +1,26 @@
-import {View, Text} from 'react-native'
+import { View, Text, FlatList, Pressable, Image } from 'react-native'
+import movies from '../data/movies'
+import Header from './Header';
 
 const MovieCards = () => {
-    return(
+    const data = movies;
+    return (
         <View>
-            <Text>MovieCards here</Text>
+            <FlatList showsVerticalScrollIndicator={false} numColumns={2} ListHeaderComponent={Header} data={data} renderItem={({ item }) => (
+                <Pressable style={{ margin: 10 }}>
+                    <Image style={{ aspectRatio: 2 / 3, height: 240 }} source={{ uri: item.image }} />
+
+                    <Text style={{ fontSize: 16, fontWeight: 'bold', width: 160 }}>{item.name.substr()}</Text>
+
+                    <Text>{item.language}</Text>
+
+                    <Text style={{ fontSize: 15 }}>{item.genre}</Text>
+
+                    <Pressable style={{ backgroundColor: "yellow", padding: 10, borderRadius: 6, width: 100, marginRight: 10, marginTop: 10 }}>
+                        <Text style={{ fontSize: 14, fontWeight: 500, textAlign: "center" }}>BOOK</Text>
+                    </Pressable>
+                </Pressable>
+            )} />
         </View>
     )
 }
