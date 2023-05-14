3436f4aa645383b110423a474b38ea1bcff0cb8e
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-14 15:46:05 +0700

    Create MovieCards.js

---
+++ Film00/components/MovieCards.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+const MovieCards = () => {
+    return(
+        <View>
+            <Text>MovieCards here</Text>
+        </View>
+    )
+}
+
+export default MovieCards
