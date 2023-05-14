9624c0e9e4f84651258746ccf2a121962072cee8
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-14 23:10:30 +0700

    Fix 2 header in HomeScreen

--- Film00/screens/HomeScreen.js
+++ Film00/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 import Header from '../components/Header'
+import MovieCards from '../components/MovieCards'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={{backgroundColor:"gray", flex:1}}>
-      <Header/>
+      
+      <MovieCards/>
     </SafeAreaView>
   )
 }
