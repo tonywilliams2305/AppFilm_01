df2fcb86c7d0afcb0451682a9e4f1d2560921009
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-21 10:24:51 -0700

    Add ticketScreen

---
+++ Film00/screens/TicketScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const TicketScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>TicketScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default TicketScreen
