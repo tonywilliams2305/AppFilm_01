60aec7c84eef3cea1ece5a25296b82630469d6ee
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-22 21:26:00 +0700

    Update index.js

--- Film00/backend/index.js
+++ Film00/backend/index.js
@@ -1,1 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const app = express()
+const Port = 8000;
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/payment', async (req,res) => {
+    try {
+        const {amount} = req.body;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'vnd',
+            payment_method_types: ['card'],
+        })
+
+        const clientSecret = paymentIntent.clientSecret
+        res.json({
+            paymentIntent: clientSecret,
+        })
+    }catch (e) {
+        console.log(e)
+    }
+})
+
+app.listen(Port, () => {
+    console.log('Listening on port 8000')
+})
