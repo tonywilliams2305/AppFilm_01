3d08c200fab31d39798611ee82794598e3f089c4
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-24 08:45:24 +0700

    Update HomeScreen.js

--- Film00/screens/HomeScreen.js
+++ Film00/screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
-import Header from '../components/Header'
 import MovieCards from '../components/MovieCards'
 
 const HomeScreen = () => {
