4356194b06b318ee1f3fca93c46c6260dcd15b87
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-24 08:42:40 +0700

    Update MovieCards.js

--- Film00/components/MovieCards.js
+++ Film00/components/MovieCards.js
@@ -2,13 +2,16 @@ import { View, Text, FlatList, Pressable, Image } from 'react-native'
 import movies from '../data/movies'
 import Header from './Header';
 import { useNavigation } from '@react-navigation/native';
-
+import { MoviesCards } from '../Context';
+import { useContext } from 'react';
+import TicketComponent from './TicketComponent';
 const MovieCards = () => {
     const data = movies;
+    const {ticket} = useContext(MoviesCards)
     const navigation = useNavigation();
     return (
         <View>
-            <FlatList showsVerticalScrollIndicator={false} numColumns={2} ListHeaderComponent={Header} data={data} renderItem={({ item }) => (
+            <FlatList showsVerticalScrollIndicator={false} numColumns={2} ListHeaderComponent={ticket.length > 0 ? TicketComponent : Header} data={data} renderItem={({ item }) => (
                 <Pressable style={{ margin: 10 }}>
                     <Image style={{ aspectRatio: 2 / 3, height: 240 }} source={{ uri: item.image }} />
 
@@ -23,8 +26,8 @@ const MovieCards = () => {
                         name: item.name,
                         image: item.image
                      })} 
-                     style={{ backgroundColor: "yellow", padding: 10, borderRadius: 6, width: 100, marginRight: 10, marginTop: 10 }}>
-                        <Text style={{ fontSize: 14, fontWeight: 500, textAlign: "center" }}>BOOK</Text>
+                     style={{ backgroundColor: "yellow", padding: 10, borderRadius: 6, width: 153, marginRight: 10, marginTop: 10 }}>
+                        <Text style={{ fontSize: 14, fontWeight: 500, textAlign: "center" }}>ĐẶT VÉ</Text>
                     </Pressable>
                 </Pressable>
             )} />
