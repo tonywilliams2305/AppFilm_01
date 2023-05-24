7c85b6090b49b6e6a713f742ef2960f21a95c512
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-24 08:44:12 +0700

    Update movies.js

--- Film00/data/movies.js
+++ Film00/data/movies.js
@@ -10,79 +10,73 @@ export default [
     {
       id: "1",
       image:
-        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ykefXRqhM7HVnLWPtXWcWCcvBy6.jpg",
-      name: "RAKSHA BANDHAN",
-      language: "Hindi",
-      genre: "ACTION",
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/wrEKXTKoUuJuLmg52wT9ZybGXEC.jpg",
+      name: "Nàng tiên cá",
+      language: "English",
+      genre: "Kinh dị",
     },
     {
       id: "2",
       image:
-        "https://www.themoviedb.org/t/p/w440_and_h660_face/6SYaaIjLDistEd7DoeS328FVWn0.jpg",
-      name: "SEETHARAMAM",
-      language: "TELUGU",
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/ksg3XSEezxpRVEB6BrKxmNOuhft.jpg",
+      name: "Sát thủ John Wick",
+      language: "English",
       genre: "DRAMA",
     },
     {
       id: "3",
       image:
-        "https://www.themoviedb.org/t/p/w440_and_h660_face/6QxZRJsODH2kNZ6oNd3qug73FoQ.jpg",
-      name: "BIMBISAARA",
-      language: "TELUGU",
-      genre: "ACTION",
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/2iwGRYzD1IlGfq2zX27uT8WBm6H.jpg",
+      name: "Ma cây trỗi dậy",
+      language: "English",
+      genre: "Hài hước, lãng mạn",
     },
     {
       id: "4",
       image:
-        "https://www.themoviedb.org/t/p/w440_and_h660_face/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg",
-      name: "BULLET TRAIN",
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/Ak5hAxorxMpxKoVw5e3kGfxs7sY.jpg",
+      name: "Vệ binh ngân hà",
       language: "ENGLISH",
-      genre: "ACTION",
+      genre: "Hành động",
     },
     {
       id: "5",
       image:
-        "https://www.themoviedb.org/t/p/w440_and_h660_face/r7XifzvtezNt31ypvsmb6Oqxw49.jpg",
-      name: "DC LEAGUE OF SUPER-PETS",
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/muzgYvvFniQnwiTdAvfMCEkHsT4.jpg",
+      name: "Baymax",
       language: "ENGLISH",
-      genre: "ANIMATION",
+      genre: "Hoạt hình",
     },
     {
       id: "6",
       image:
-        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/a2oUkUgMowYyi8qeGS28rAXPNXT.jpg",
-      name: "KURUTHI AATTAM",
-      language: "TAMIL",
-      genre: "ACTION",
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/ccaqnG8K0ub279MX19F2RPsEwal.jpg",
+      name: "Khắc tinh của quỷ",
+      language: "English",
+      genre: "Kinh dị",
     },
     {
       id: "7",
       image:
-        "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/amAbP9YbIAlAXHdIon1x8l7aDFT.jpg",
-      name: "EK VILLIAN RETURNS",
-      language: "HINDI",
-      genre: "ACTION",
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/owt8hAoRxyK2tkLj9G0nh0r3Vpr.jpg",
+      name: "Săn lùng",
+      language: "English",
+      genre: "Hành động",
     },
     {
       id: "8",
       image:
-        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qlsNqqczg1rr5lfUgiPlLiykGGM.jpg",
-      name: "VIRUMAN",
-      language: "TAMIL",
-      genre: "ACTION",
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/dKjjVuPawiREBYjREb3U5SCtfrt.jpg",
+      name: "Mèo đi hia",
+      language: "English",
+      genre: "Hoạt hình",
     },
     {
         id:"9",
-        image:"https://www.themoviedb.org/t/p/w440_and_h660_face/xAG7S6X73B74CeLjo8gcVoWtunr.jpg",
-        name:"ROCKETRY",
+        image:"https://www.themoviedb.org/t/p/w220_and_h330_face/15dkfYdK08TJgx33LlBB694fUGS.jpg",
+        name:"Vong hồn rước lễ",
         language:"HINDI",
         genre: "DRAMA",
     },
-    {
-        id:"10",
-        image:"https://www.themoviedb.org/t/p/w188_and_h282_bestv2/irmHAELoYRaECBr8zQAlD4myrfk.jpg",
-        name:"777 CHARLIE",
-        language: "KANNADA",
-        genre: "DRAMA",
-    }
+    
   ];
