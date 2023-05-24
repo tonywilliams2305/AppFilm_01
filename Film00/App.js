a41945fead4866a578d0d5a230caa50d1b9dcadc
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-05-24 08:46:18 +0700

    Update App.js

--- Film00/App.js
+++ Film00/App.js
@@ -3,12 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import StackNavigator from './StackNavigator';
 import { MovieContext } from './Context';
+import { StripeProvider } from '@stripe/stripe-react-native';
 export default function App() {
   return (
     <>
       <MovieContext>
-        <StackNavigator />
-        <StatusBar style="auto" />
+        <StripeProvider publishableKey='pk_test_51N9rxbGNEwXQC17lfE44gnaQICdOPJPoLKFPpmvSa5iNmHoNyfex4OKFAzFcd8ViJqVCLNOrIqMZrNDlzw2nEuVK007h6quzav'>
+          <StackNavigator />
+          <StatusBar style="auto" />
+        </StripeProvider>
+
       </MovieContext>
 
     </>
